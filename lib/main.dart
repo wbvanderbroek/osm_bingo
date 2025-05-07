77daa8e2b9b19767b2748340b2a688b5dddda63a
Walter van der Broek <wbvanderbroek@st.noorderpoort.nl>
2025-05-07 10:15:58 +0200

    Cleanup: Removed old imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:osm_bingo/map.dart';
 
 void main() {
