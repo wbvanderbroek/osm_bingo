9f5f36b27c5ac4b19051a15a6916729df14aeaaa
MikeOomen <mike.oomen1@gmail.com>
2025-05-07 15:41:33 +0200

    Create bingo_card.dart
    Made a 2 dimensional list of locations

---
+++ lib/bingo_logic/bingo_card.dart
@@ -0,0 +1,77 @@
+import 'bingo_element.dart';
+
+class BingoCard {
+  List<List<BingoElement>> bingoCard = [
+    [
+      BingoElement("Het Kasteel", 53.21774270053335, 6.555017571423083),
+      BingoElement("Forum Groningen", 53.21893636399295, 6.570391954630532),
+      BingoElement("Martinitoren", 53.21938568974004, 6.568207623222464),
+      BingoElement("Nieuwe Kerk", 53.223280467862814, 6.561681503979227),
+      BingoElement("Groningen Museum", 53.21228190017462, 6.566079351512879),
+    ],
+    [
+      BingoElement("AKerk", 53.216492401093994, 6.562364669840671),
+      BingoElement("Groningen Station", 53.211020494728324, 6.564063342704337),
+      BingoElement("MartiniPlaza", 53.20303240513857, 6.555081707361252),
+      BingoElement(
+        "Noorderpoort Kunst & media",
+        53.207594630285534,
+        6.557172995605856,
+      ),
+      BingoElement("Kinderboedrij stads", 53.20606466399597, 6.547321014823603),
+    ],
+    [
+      BingoElement(
+        "Skatepark De Paardenbak",
+        53.20415683364296,
+        6.5478241337411,
+      ),
+      BingoElement("Groningen Atletiek", 53.198871227315074, 6.539539240983704),
+      BingoElement(
+        "Noorderpoort Technologie & ICT",
+        53.20329518505192,
+        6.562301116096836,
+      ),
+      BingoElement("Pepergasthuis", 53.21742607496547, 6.57141301267729),
+      BingoElement(
+        "Rijksuniversiteit Groningen",
+        53.21920524542988,
+        6.563175805756274,
+      ),
+    ],
+    [
+      BingoElement("Noorderplantsoen", 53.22421158564376, 6.555581019774128),
+      BingoElement(
+        "McDonald's Westerhaven",
+        53.21619848026004,
+        6.557473848143307,
+      ),
+      BingoElement(
+        "McDonald's Herenstraat",
+        53.21652939251069,
+        6.567871745345571,
+      ),
+      BingoElement(
+        "McDonald's Sontplein",
+        53.216157759482684,
+        6.584330600549564,
+      ),
+      BingoElement("KFC Westerkade", 53.21524576553252, 6.557393169762863),
+    ],
+    [
+      BingoElement("Prinsentuin", 53.2213457069883, 6.568897712273988),
+      BingoElement(
+        "Voormalig Drafbaan Stadspark",
+        53.20349208509578,
+        6.5491170434165795,
+      ),
+      BingoElement(
+        "Domino's Pizza Paterswoldseweg",
+        53.203118573549695,
+        6.559126479689959,
+      ),
+      BingoElement("'t Pannekoekschip", 53.21695900180443, 6.579041728818073),
+      BingoElement("Boteringebrug", 53.22132573050829, 6.562735226033523),
+    ],
+  ];
+}
