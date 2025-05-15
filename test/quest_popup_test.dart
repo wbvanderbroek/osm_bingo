1824a0d8cddfbad7cb47de0b20474b652f5a1ebd
Walter van der Broek <wbvanderbroek@st.noorderpoort.nl>
2025-05-15 09:49:25 +0200

    Chore: Added one more popup test

--- test/quest_popup_test.dart
+++ test/quest_popup_test.dart
@@ -9,33 +9,65 @@ import 'package:osm_bingo/navigation_service.dart';
 import 'package:osm_bingo/screens/map.dart';
 
 void main() {
-  testGoldens('QuestPopup', (tester) async {
-    MapService().currentPosition = LatLng(
-      BingoCard.flattenedBingoElements[0].latitude,
-      BingoCard.flattenedBingoElements[0].longitude,
-    );
+  group('QuestPopup', () {
+    testGoldens('QuestPopup', (tester) async {
+      MapService().currentPosition = LatLng(
+        BingoCard.flattenedBingoElements[0].latitude,
+        BingoCard.flattenedBingoElements[0].longitude,
+      );
 
-    await tester.pumpWidget(
-      MaterialApp(
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      await tester.pumpWidget(
+        MaterialApp(
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          ),
+          navigatorKey: navigatorKey,
+
+          home: const OpenStreetMapScreen(),
         ),
-        navigatorKey: navigatorKey,
+      );
+      InRangeChecker().checkLocation(
+        MapService().currentPosition.latitude,
+        MapService().currentPosition.longitude,
+      );
 
-        home: const OpenStreetMapScreen(),
-      ),
-    );
-    InRangeChecker().checkLocation(
-      BingoCard.flattenedBingoElements[0].latitude,
-      BingoCard.flattenedBingoElements[0].longitude,
-    );
+      await tester.loadFonts();
+      await tester.pumpAndSettle();
+      tester.useFuzzyComparator(allowedDiffPercent: 0.005);
+      await expectLater(
+        find.byType(MaterialApp),
+        matchesGoldenFile('quest_popup.png'),
+      );
+    });
 
-    await tester.loadFonts();
-    await tester.pumpAndSettle();
-    tester.useFuzzyComparator(allowedDiffPercent: 0.005);
-    await expectLater(
-      find.byType(MaterialApp),
-      matchesGoldenFile('quest_popup.png'),
-    );
+    testGoldens('NoQuestPopup', (tester) async {
+      MapService().currentPosition = LatLng(
+        BingoCard.flattenedBingoElements[0].latitude + 5.55,
+        BingoCard.flattenedBingoElements[0].longitude + 3.11,
+      );
+
+      await tester.pumpWidget(
+        MaterialApp(
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          ),
+          navigatorKey: navigatorKey,
+
+          home: const OpenStreetMapScreen(),
+        ),
+      );
+      InRangeChecker().checkLocation(
+        MapService().currentPosition.latitude,
+        MapService().currentPosition.longitude,
+      );
+
+      await tester.loadFonts();
+      await tester.pumpAndSettle();
+      tester.useFuzzyComparator(allowedDiffPercent: 0.005);
+      await expectLater(
+        find.byType(MaterialApp),
+        matchesGoldenFile('no_quest_popup.png'),
+      );
+    });
   });
 }
